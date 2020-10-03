3caeefacc5f5f821a974940cf34ede97a0e247e3
corrado <corradolongoni@Corrados-MacBook-Pro.local>
2020-10-04 00:12:03 +0200

    added corpse catcher

---
+++ Assets/Scripts/Player/PlayerCorpseCatcher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCorpseCatcher : MonoBehaviour
+{
+    [SerializeField] private int corpseCount;
+
+    private void OnEnable()
+    {
+        corpseCount = 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var life = collision.gameObject.GetComponent<CorpseLife>();
+
+        if (life != null)
+        {
+            life.Die();
+            corpseCount++;
+        }
+    }
+}
