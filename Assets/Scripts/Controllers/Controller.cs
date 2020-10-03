bd73eb12533ecafbe7c71633073fccc04bac6a7f
corrado <corradolongoni@Corrados-MacBook-Pro.local>
2020-10-03 19:04:42 +0200

    fixes on controller

--- Assets/Scripts/Controllers/Controller.cs
+++ Assets/Scripts/Controllers/Controller.cs
@@ -29,4 +29,10 @@ public class Controller : MonoBehaviour
             if(animator != null) animator.SetInteger("state", value);
         }
     }
+
+    private void OnEnable()
+    {
+        AnimatorState = -1;
+        AnimatorState = 0;
+    }
 }
