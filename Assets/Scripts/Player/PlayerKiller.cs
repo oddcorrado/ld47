16b56560ca1f3975dca6c5871e88199c34fe550f
jeanfrancoisemil <50072047+jeanfrancoisemil@users.noreply.github.com>
2020-10-03 23:35:22 +0200

    PlayerKiller

---
+++ Assets/Scripts/Player/PlayerKiller.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKiller : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var life = collision.gameObject.GetComponent<PlayerLife>();
+
+        Debug.Log("PlayerKiller:  " + collision.gameObject + " " + life);
+        if (life != null) life.Die();
+    }
+}
