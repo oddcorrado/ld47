9b5e5c044f9f8948ec606ce9a8a17f552f6fe5e7
corrado <corradolongoni@Corrados-MacBook-Pro.local>
2020-10-03 23:22:19 +0200

    corpses and player life

---
+++ Assets/Scripts/Life/CorpseLife.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorpseLife : MonoBehaviour
+{
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Life/PlayerLife.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLife : MonoBehaviour
+{
+    [SerializeField] Transform spawnPoint;
+
+    private StateManager stateManager;
+
+    void Start()
+    {
+        if (spawnPoint == null) spawnPoint = transform;
+        stateManager = FindObjectOfType<StateManager>();
+    }
+
+    public void Die()
+    {
+        transform.position = spawnPoint.position;
+        stateManager.StartWheel();
+    }
+}
