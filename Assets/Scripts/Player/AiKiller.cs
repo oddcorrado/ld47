77caa5b53df218a001b71fe52db063ef27890261
oddarock <corradolongoni@gmail.com>
2020-10-04 16:26:00 +0200

    cockraoch dart

--- Assets/Scripts/Player/AiKiller.cs
+++ Assets/Scripts/Player/AiKiller.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class AiKiller : MonoBehaviour
 {
+    [SerializeField] Animator dartAnimator;
+
+    private Coroutine coroutine;
+
+
+    IEnumerator AnimationTimer()
+    {
+        dartAnimator.SetInteger("state", 1);
+        yield return new WaitForSeconds(1);
+        dartAnimator.SetInteger("state", 0);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         var life = collision.gameObject.GetComponent<AILife>();
 
-        Debug.Log("AiKiller:  " + collision.gameObject + " " + life);
-        if (life != null) life.Die();
+        if (life != null)
+        {
+            if (coroutine != null) StopCoroutine(coroutine);
+            coroutine = StartCoroutine(AnimationTimer());
+
+            life.Die();
+        }
     }
 }
