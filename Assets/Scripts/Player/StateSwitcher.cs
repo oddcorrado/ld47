d43afee768a9a879d7efdda7eed8519ff4763dd2
oddarock <corradolongoni@gmail.com>
2020-10-05 00:22:47 +0200

    tuto and state switcher

---
+++ Assets/Scripts/Player/StateSwitcher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateSwitcher : MonoBehaviour
+{
+    private StateManager stateManager;
+    [SerializeField] private LoopState forcedState;
+
+    IEnumerator Start()
+    {
+        yield return null;
+        stateManager = FindObjectOfType<StateManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        {
+            var life = collision.gameObject.GetComponent<PlayerLife>();
+
+            Debug.Log("PlayerKiller:  " + collision.gameObject + " " + life);
+            stateManager.ForceState(forcedState);
+            Destroy(gameObject);
+        }
+    }
+}
