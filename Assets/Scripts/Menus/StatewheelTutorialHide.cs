5cf01a1e9e82279eb0e2f955c6d421f96e3b1c21
Gnarf <vitali.roubtsov@hotmail.fr>
2020-10-05 13:27:25 +0200

    statewheel tutorial window

---
+++ Assets/Scripts/Menus/StatewheelTutorialHide.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class StatewheelTutorialHide : MonoBehaviour
+{
+    void Update()
+    {
+        if(Keyboard.current.enterKey.IsPressed())
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
