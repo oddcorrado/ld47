0de2d9766522aa295517bcb1a481f97e7bca0bf6
oddarock <corradolongoni@gmail.com>
2020-10-06 00:16:53 +0200

    last minute dirty fcking fixes

---
+++ Assets/Scripts/Props/LevelEnd.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelEnd : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var life = collision.gameObject.GetComponent<PlayerLife>();
+
+        Debug.Log("PlayerKiller:  " + collision.gameObject + " " + life);
+
+        if (life != null)
+        {
+            if (SceneManager.GetActiveScene().buildIndex < SceneManager.sceneCount - 1)
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            else
+                SceneManager.LoadScene("MenuLevels");
+        }        
+    }
+}
