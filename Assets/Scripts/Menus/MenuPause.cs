0184738d3e9aa445551e6fdcfd0c094b4da029f3
oddarock <corradolongoni@gmail.com>
2020-10-05 21:24:25 +0200

    pause menu

---
+++ Assets/Scripts/Menus/MenuPause.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPause : MonoBehaviour
+{
+    [SerializeField] GameObject pauseButton;
+
+    private void OnEnable()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        pauseButton.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        pauseButton.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MenuLevels");
+        pauseButton.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
